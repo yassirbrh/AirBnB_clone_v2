b1113ca7fac54a7c11b73d1a80af2a38371b8953
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-09-24 02:06:13 +0100

    Add DBStorage class and some modifications to the BaseModel class and for City, State, __init__.

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from models.city import City
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+import shlex
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        mdls = models.storage.all()
+        first_list = []
+        final_list = []
+        for key in mdls:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                first_list.append(mdls[key])
+        for elem in first_list:
+            if (elem.state_id == self.id):
+                final_list.append(elem)
+        return final_list
