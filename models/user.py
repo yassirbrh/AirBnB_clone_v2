7a7725bc6fcf6aa49fe93ac29fe46eb4364b7a03
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-09-24 02:10:40 +0100

    Modify the User Class.

--- models/user.py
+++ models/user.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
