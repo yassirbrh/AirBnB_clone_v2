94977f017e190edbe527e9275d3b048e1ce09801
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-10-05 19:12:29 +0100

    Bash script that sets up the web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,28 +14,24 @@ echo "<html>
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
-printf %s "http {
-        root /var/www/helloworld;
-        index index.html;
-        server {
-                listen 80;
-                add_header X-Served-By $HOSTNAME;
-                location \ {
-                }
-                location /redirect_me {
-                	return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-                }
-                error_page 404 /404_redirection;
-                location = /404_redirection {
-                        internal;
-                        return 404 \"Ceci n'est pas une page\n\n\";
-                }
-                location /hbnb_static {
-                	alias /data/web_static/current;
-                	index index.html;
-                }
-        }
-}
-events {
+printf %s "server {
+            listen 80 default_server;
+            add_header X-Served-By $HOSTNAME;
+            root /var/www/html;
+            index index.html;
+            location \ {
+            }
+            location /redirect_me {
+            	return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+            }
+            error_page 404 /404_redirection;
+            location = /404_redirection {
+                    internal;
+                    return 404 \"Ceci n'est pas une page\n\n\";
+            }
+            location /hbnb_static {
+            	alias /data/web_static/current;
+            	index index.html;
+            }
 }" > /etc/nginx/nginx.conf
 service nginx restart
