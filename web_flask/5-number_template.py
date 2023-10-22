3c388a9f8fbb1a3f97d2efb4e0b9085c5ba74757
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-10-22 11:15:33 +0100

    Web application using Flask, adding the template rendering in the app.

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -34,5 +34,10 @@ def number(n):
     return str(n) + ' is a number'
 
 
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template("5-number.html", n=n)
+
+
 if __name__ == '__main__':
     app.run()
