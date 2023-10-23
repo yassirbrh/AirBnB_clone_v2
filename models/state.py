e9e68062c9353000c6bc8471786d06f7be1aace3
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-10-23 13:23:57 +0100

    Update the State class.

--- models/state.py
+++ models/state.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 import models
 import shlex
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -16,17 +17,18 @@ class State(BaseModel, Base):
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
 
-    @property
-    def cities(self):
-        mdls = models.storage.all()
-        first_list = []
-        final_list = []
-        for key in mdls:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                first_list.append(mdls[key])
-        for elem in first_list:
-            if (elem.state_id == self.id):
-                final_list.append(elem)
-        return final_list
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            mdls = models.storage.all()
+            first_list = []
+            final_list = []
+            for key in mdls:
+                city = key.replace('.', ' ')
+                city = shlex.split(city)
+                if (city[0] == 'City'):
+                    first_list.append(mdls[key])
+            for elem in first_list:
+                if (elem.state_id == self.id):
+                    final_list.append(elem)
+            return final_list
